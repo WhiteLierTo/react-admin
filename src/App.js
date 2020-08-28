172d3cf5ef2d4e7cbe1c1c3a47ad4a8a7199289b
lichao <1932018667@qq.com>
2020-08-28 14:33:48 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Home from "./views/Home.js";
+import About from "./views/About.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route component={Home} exact path="/" />
+          <Route component={About} exact path="/about" />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
---
+++ src/views/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>关于</div>;
+  }
+}
